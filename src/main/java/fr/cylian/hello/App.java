72da68ef18af1de817d4aa3372482129bdc322d3
Cyril Pfaff <cp512@cylian.org>
2021-10-07 11:08:24 +0200

    Initial release

---
+++ src/main/java/fr/cylian/hello/App.java
@@ -0,0 +1,27 @@
+package fr.cylian.hello;
+
+import java.lang.String;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args)
+ {
+        System.out.println("Hello Le monde - v7!");
+
+        App.main2(args);
+        App.main2(args);
+        App.main2(args);
+        App.main2(args);
+        App.main2(args);
+
+        return;
+    }
+    public static void main2(String[] args)
+ {
+        System.out.println("Hello Le monde - v7!");
+        return;
+    }
+}
