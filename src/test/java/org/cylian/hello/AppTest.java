14e847f7c51013cb946584994798fd0a9aa651da
Cyril Pfaff <cp512@cylian.org>
2021-10-08 11:46:03 +0200

    Plantage de tests

--- src/test/java/org/cylian/hello/AppTest.java
+++ src/test/java/org/cylian/hello/AppTest.java
@@ -7,13 +7,37 @@ import org.junit.Test;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
+public class AppTest
 {
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
+    public void shouldAnswerWithTrue1()
+    {
+        assertTrue( true );
+    }
+
+    @Test
+    public void shouldAnswerWithTrue2()
+    {
+        assertTrue( false );
+    }
+
+    @Test
+    public void shouldAnswerWithTrue3()
+    {
+        assertTrue( true );
+    }
+
+    @Test
+    public void shouldAnswerWithTrue4()
+    {
+        assertTrue( false );
+    }
+
+    @Test
+    public void shouldAnswerWithTrue5()
     {
         assertTrue( true );
     }
