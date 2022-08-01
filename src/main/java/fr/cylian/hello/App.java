3a7a9c9c2e4a4096b710f521ed9c67f264c53c6b
Cyril-Pfaff <cyril.pfaff@gmail.com>
2022-08-01 16:15:15 +0200

    Update App.java

--- src/main/java/fr/cylian/hello/App.java
+++ src/main/java/fr/cylian/hello/App.java
@@ -9,7 +9,7 @@ import java.lang.String;
 public class App {
     public static void main(String[] args)
     {
-        System.out.println("Hello World - v3");
+        System.out.println("Hello World - v4");
         return;
     }
 }
