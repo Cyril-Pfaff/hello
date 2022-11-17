0a8c99ed051a0a5613bf27c3e54211028fd2d1a2
Cyril Pfaff <cp512@cylian.org>
2022-11-17 15:37:21 +0100

    Gros changement

--- src/main/java/fr/cylian/hello/App.java
+++ src/main/java/fr/cylian/hello/App.java
@@ -9,7 +9,7 @@ import java.lang.String;
 public class App {
     public static void main(String[] args)
     {
-        System.out.println("Hello World - v5");
+        System.out.println("Hello World - v6");
         return;
     }
 }
